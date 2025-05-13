51e3f85ad13d052ec9f21f93f7172a3d2f019d53
AfkaraLP <kijo@gmx.net>
2025-05-13 12:38:01 -0500

    add styles and stuff also flake but like brokem

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            rust-analyzer
+            clang
+            pkg-config
+            lld
+            eza
+            fd
+            wasm-pack
+            wasm-bindgen-cli
+            (
+              rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+                extensions = [ "rust-src" "rustfmt-preview" "clippy-preview" ];
+                targets = [ "arm-unknown-linux-gnueabihf" ];
+              })
+            )
+          ];
+
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+          '';
+        };
+      }
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
+// use lol_alloc::LockedAllocator;
 use once_cell::sync::Lazy;
 use reqwest::{Client, header::ACCEPT};
 use yew::prelude::*;
+// use std::sync::{Arc, Mutex};
+// use yew::{html::IntoPropValue, prelude::*};
+//
+// struct UseMutRef<F> {
+//     init_fn: F,
+// }
+//
+// impl<T: 'static, F: FnOnce() -> T> Hook for UseMutRef<F> {
+//     type Output = Arc<Mutex<T>>;
+//     fn run(self, ctx: &mut yew::HookContext) -> Self::Output {
+//         Arc::new(Mutex::new(ctx.into_prop_value()))
+//     }
+// }
+//
+// pub fn use_async_hook<T: 'static, F>(init_fn: F) -> impl Hook<Output = Arc<Mutex<T>>>
+// where
+//     F: FnOnce() -> T,
+// {
+//     todo!();
+// }
 
 static CLIENT: Lazy<Client> = Lazy::new(Client::new);
 
 #[allow(non_snake_case)]
 #[function_component]
 fn App() -> Html {
-    let dad_joke_content = Box::leak(Box::new(use_state(|| "dadjoke".to_owned())));
+    let dad_joke_content = Box::leak(Box::new(use_state(|| {
+        "yes I'm testing in prod don't @ me".to_owned()
+    })));
     let onclick = {
-        let get_dad_joke = |ev: MouseEvent| {
+        let get_dad_joke = |_ev: MouseEvent| {
             wasm_bindgen_futures::spawn_local(async {
                 let dad_joke = CLIENT
                     .get("https://icanhazdadjoke.com/")
